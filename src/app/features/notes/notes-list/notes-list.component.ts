9141efc248d2d184715e1c5d7dbdffa3be0c28ce
alex <109907113+alex-faryna@users.noreply.github.com>
2022-09-01 15:54:02 +0300

    improved layout

--- src/app/features/notes/notes-list/notes-list.component.ts
+++ src/app/features/notes/notes-list/notes-list.component.ts
@@ -1,24 +1,146 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef, NgZone, OnInit,
+  QueryList,
+  ViewChildren
+} from '@angular/core';
 import {NotesService} from "../../../shared/services/notes.service";
+import {NoteListItemComponent} from "./note-list-item/note-list-item.component";
+import {Note} from "../../../shared/models/note.model";
+import {BehaviorSubject, delay, startWith} from "rxjs";
+
+const OFFSET = 30;
+
+enum InsertMode {
+  right,
+  bottom,
+  done
+}
 
 @Component({
   selector: 'app-notes-list',
   templateUrl: './notes-list.component.html',
-  styleUrls: ['./notes-list.component.scss']
+  styleUrls: ['./notes-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NotesListComponent implements OnInit {
+export class NotesListComponent implements OnInit, AfterViewInit {
+  @ViewChildren('noteCard') private notesCards!: QueryList<NoteListItemComponent>;
 
   public colsCount = 3;
   public notes$ = this.notesService.getNotesList();
-
   public get cols(): number[] {
     return [...Array(this.colsCount).keys()];
   }
 
-  constructor(private notesService: NotesService) { }
+  private observer!: MutationObserver;
+  public availableSpots: number[][] = [];
 
-
-  ngOnInit(): void {
+  constructor(private notesService: NotesService,
+              private cdr: ChangeDetectorRef,
+              private zone: NgZone,
+              private ref: ElementRef) {
   }
 
+  public ngOnInit(): void {
+    //this.observer = new MutationObserver(() => { this.updateView() });
+    // this.observer.observe(this.ref.nativeElement, {childList: true});
+  }
+
+  public ngAfterViewInit(): void {
+    this.notesCards.changes.pipe(
+      startWith(this.notesCards),
+    ).subscribe(() => this.updateView());
+  }
+
+  public trackByMethod(index: number, el: Note): number {
+    return el.id;
+  }
+
+  private updateView(): void {
+    console.log("update view");
+    let firstRow = true;
+    const notesContainerWidth = this.ref.nativeElement.clientWidth;
+    for (let note of this.ref.nativeElement.children) {
+      if (this.availableSpots.length === 0) {
+        this.availableSpots = [[note.clientWidth, 0], [0, note.clientHeight]];
+        console.log(this.availableSpots);
+        this.cdr.markForCheck();
+        continue;
+      }
+
+      const baseY = this.availableSpots[0][1];
+      const baseSpots: number[][] = [];
+      for (const spot of this.availableSpots) {
+        if (spot[1] > baseY + OFFSET) {
+          break;
+        }
+        baseSpots.push(spot);
+      }
+
+      const spot = baseSpots.reduce((prev, curr) =>
+        prev[0] < curr[0] ? prev : curr);
+
+      const left = spot[0];
+      const top = spot[1];
+      note.style.left = `${left}px`;
+      note.style.top = `${top}px`;
+
+      // remove used spot
+      let idx = 0;
+      for (const [i, temp] of this.availableSpots.entries()) {
+        if (temp[0] === spot[0] && temp[1] === spot[1]) {
+          idx = i;
+          break;
+        }
+      }
+      this.availableSpots.splice(idx, 1);
+
+      const noteWidth = note.clientWidth;
+      const rightSpot = [left + noteWidth, top];
+      const bottomSpot = [left, top + note.clientHeight];
+      const noteRightSide = noteWidth + note.offsetLeft;
+      console.dir(note);
+      console.log(notesContainerWidth);
+      console.log(noteRightSide);
+
+      // we have got a bit conclusion: only the first row can put notes to the right,
+      // other rows can only do this below them
+      if (firstRow && noteRightSide + noteWidth > notesContainerWidth) {
+        firstRow = false;
+      }
+
+      let insertMode = firstRow
+        ? InsertMode.right
+        : InsertMode.bottom;
+      for (const [i, temp] of this.availableSpots.entries()) {
+        if (insertMode === InsertMode.right) {
+          if (rightSpot[1] < temp[1]) {
+            this.availableSpots.splice(i, 0, rightSpot);
+            insertMode = InsertMode.bottom;
+          } else if (rightSpot[1] === temp[1] && rightSpot[0] <= temp[0]) {
+            this.availableSpots.splice(i, 0, rightSpot);
+            insertMode = InsertMode.bottom;
+          }
+        } else if (insertMode === InsertMode.bottom) {
+          if (bottomSpot[1] < temp[1]) {
+            this.availableSpots.splice(i, 0, bottomSpot);
+            insertMode = InsertMode.done;
+          } else if (bottomSpot[1] === temp[1] && bottomSpot[0] <= temp[0]) {
+            this.availableSpots.splice(i, 0, bottomSpot);
+            insertMode = InsertMode.done;
+          }
+        }
+      }
+      if (insertMode === InsertMode.right) {
+        this.availableSpots.push(rightSpot, bottomSpot);
+      } else if(insertMode === InsertMode.bottom) {
+        this.availableSpots.push(bottomSpot);
+      }
+      console.log(this.availableSpots);
+      this.cdr.markForCheck();
+    }
+  }
 }
--- src/app/shared/services/notes.service.ts
+++ src/app/shared/services/notes.service.ts
@@ -12,67 +12,83 @@ export class NotesService {
   public getNotesList(): Observable<Note[]> {
     return of([
       {
+        id: 0,
         title: "Note 1",
         content: "Content 1",
       },
       {
+        id: 1,
         title: "Note 2",
         content: "Content 2",
-      },
-      {
-        title: "Note 3",
-        content: "Content 1",
         test: "2",
       },
       {
+        id: 2,
+        title: "Note 3",
+        content: "Content 1",
+      },
+      {
+        id: 3,
         title: "Note 4",
         content: "Content 2",
         test: "2",
       },
       {
+        id: 4,
         title: "Note 5",
         content: "Content 1",
+        test: "2",
+        test2: "2",
       },
       {
+        id: 5,
         title: "Note 6",
         content: "Content 2",
         test: "2",
       },
       {
+        id: 6,
         title: "Note 7",
         content: "Content 1",
         test: "2",
       },
       {
+        id: 7,
         title: "Note 8",
         content: "Content 2",
       },
       {
+        id: 8,
         title: "Note 9",
         content: "Content 2",
       },
       {
+        id: 9,
         title: "Note 10",
         content: "Content 2",
       },
       {
+        id: 10,
         title: "Note 11",
         content: "Content 2",
         test: "3"
       },
       {
-        title: "Note 9",
+        id: 11,
+        title: "Note 12",
         content: "Content 2",
       },
       {
-        title: "Note 10",
+        id: 12,
+        title: "Note 13",
         content: "Content 2",
       },
       {
-        title: "Note 11",
+        id: 13,
+        title: "Note 14",
         content: "Content 2",
         test: "3"
       }
-    ]).pipe(delay(1300));
+    ].slice(0, 14));
   }
 }
